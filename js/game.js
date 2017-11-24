190a8f9d575fc496900b29366283f5cf20a713b1
Jan Schulte <hello@unexpected-co.de>
2017-11-24 12:54:54 +0100

    We kill tessa if she jumps out of the game field

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ function preload() {
 
     game.load.image('sky', 'assets/sky.png');
     game.load.image('platform', 'assets/platform.png');
-    game.load.image('ground', 'assets/ground.png');
+
     game.load.image('star', 'assets/star.png');
     game.load.image('tessa', 'assets/tessa.png');
 }
@@ -26,20 +26,10 @@ function create() {
     //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
 
-    //  The platformsGroup group contains the ground and the 2 ledges we can jump on
     platformsGroup = game.add.group();
 
     //  We will enable physics for any object that is created in this group
     platformsGroup.enableBody = true;
-
-    // Here we create the ground.
-    var ground = platformsGroup.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
 
     //  Now let's create two ledges
     var ledge = platformsGroup.create(150, 450, 'platform');
@@ -52,7 +42,8 @@ function create() {
     ledge.body.immovable = true;
 
     // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'tessa');
+    // player = game.add.sprite(32, game.world.height - 150, 'tessa');
+    player = game.add.sprite(150, 150, 'tessa');
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
@@ -60,7 +51,10 @@ function create() {
     //  Player physics properties. Give the little guy a slight bounce.
     player.body.bounce.y = 0.1;
     player.body.gravity.y = 200;
-    player.body.collideWorldBounds = true;
+    // player.body.collideWorldBounds = true;
+
+    player.checkWorldBounds = true;
+    player.events.onOutOfBounds.add(killPlayer, this);
 
     //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
@@ -97,9 +91,13 @@ function update() {
         player.frame = 4;
     }
 
-    //  Allow the player to jump if they are touching the ground.
     if (player.body.touching.down)
     {
         player.body.velocity.y = -350;
     }
+}
+
+function killPlayer(player) {
+  player.kill();
+  const scoreText = game.add.text(50, 50, 'Game over', { fontSize: '32px', fill: '#000' });
 }
